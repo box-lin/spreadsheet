eb924be617cc2d23714420ddf894c8155452432f
Boxiang Lin <boxiang.lin@wsu.edu>
2021-09-14 10:41:31 -0700

    Applied StyleCop.Analyzers into the projects, do modifications to clear the warnings

--- HW2/Utils.cs
+++ HW2/Utils.cs
@@ -1,63 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Utils.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace HW2
 {
+    using System;
+    using System.Collections.Generic;
+
     /// <summary>
-    /// This the class of the collection of methods
+    /// This the class of the collection of methods.
     /// </summary>
     public class Utils
     {
+        /// <summary>
+        /// The method to generate and return a random list with boundary of element value and size of the list specified.
+        /// </summary>
+        /// <param name="min"> min boundary. </param>
+        /// <param name="max"> max boundary. </param>
+        /// <param name="size"> size of the list. </param>
+        /// <returns> return a int type random list. </returns>
         public static List<int> GenerateRandomList(int min, int max, int size)
         {
             Random rd = new Random();
             List<int> rdList = new List<int>();
-            for (int i = 0; i<size; i++)
+            for (int i = 0; i < size; i++)
             {
-                rdList.Add(rd.Next(min, max+1)); //[min,max+1) = [min,max] based on integer min diff = 1
+                rdList.Add(rd.Next(min, max + 1)); // [min,max+1) = [min,max] based on integer min diff = 1
             }
+
             return rdList;
         }
 
+        /// <summary>
+        /// The method that calculate the numbers of distinct value through hashset.
+        /// </summary>
+        /// <param name="rdList"> pass in a random list. </param>
+        /// <returns> return a number of distinct value. </returns>
         public static int HashSetDistinct(List<int> rdList)
         {
             HashSet<int> hash = new HashSet<int>();
-            foreach (int val in rdList){
+            foreach (int val in rdList)
+            {
                 hash.Add(val);
             }
+
             return hash.Count;
         }
 
         /// <summary>
-        /// This should be a O(N^2) worst time compleixty and O(1) space. 
+        /// This should be a O(N^2) worst time compleixty and O(1) space.
         /// </summary>
-        /// <param name="rdList"></param>
-        /// <returns></returns>
+        /// <param name="rdList"> pass in a random list. </param>
+        /// <returns> return a number of distinct value. </returns>
         public static int ConstantSpaceDistinct(List<int> rdList)
         {
-            if (rdList.Count < 2) return rdList.Count; //empty or 1 
-            
-            int res = rdList.Count;
-            for (int i = 0; i < rdList.Count-1; i++) //O(N) is a must in best,average,worst cases
+            if (rdList.Count < 2)
             {
-                for (int j = i+1; j < rdList.Count; j++) //O(N) happens in worst case "no duplicates"
-                {
-                    if(rdList[i] == rdList[j]) //O(1) Cosntant
-                    {
-                        //when we found a duplicate in j(fast pointer) we decrease the res by 1;
-                        //we also want to break and start i(slow pointer) next step because we 
-                        //sure that we will eventually encounter this duplicate and will keep the 
-                        //pattern that search for future duplicates
+                return rdList.Count; // empty or 1 size of list, just return list size.
+            }
+
+            // About O(N*N*1) = O(N^2) runtime.
+            int res = rdList.Count;
+            for (int i = 0; i < rdList.Count - 1; i++)
+            {// O(N) is a must in best,average,worst cases.
+                for (int j = i + 1; j < rdList.Count; j++)
+                {// O(N) happens in worst case "no duplicates".
+                    if (rdList[i] == rdList[j])
+                    {// O(1) Cosntant
+                        /* When we found a duplicate in j(fast pointer) we decrease the res by 1;
+                         * We want to break and start i(slow pointer) next step because we sure that we
+                         * will eventually encounter this duplicate again and will keep the pattern that
+                         * search for future duplicates.
+                         */
                         res--;
                         break;
                     }
-                } 
+                }
             }
+
             return res;
         }
-
     }
 }
--- HW2UnitTests/TestUtils.cs
+++ HW2UnitTests/TestUtils.cs
@@ -1,24 +1,25 @@
-﻿// NUnit 3 tests
-// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
-using NUnit.Framework;
-using System.Collections;
-using System.Collections.Generic;
-using HW2;
-using System.Linq;
+﻿// <copyright file="TestUtils.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace HW2UnitTests
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using HW2;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The is the Test Class that test all static methods for the HW2 project Utils class.
+    /// </summary>
     [TestFixture]
     public class TestUtils
     {
+        // Class member random List variable to be used in the test cases.
+        private List<int> rdList;
 
-        //Class member random List variable to be used in the test cases
-        List<int> rdList;
-
-        //Class variable to hold the expect distinct number from random list.
-        int expectDistinct;
-        
-
+        // Class variable to hold the expect distinct number from random list.
+        private int expectDistinct;
 
         /// <summary>
         /// random list instantiate when run the test.
@@ -26,23 +27,21 @@ namespace HW2UnitTests
         [SetUp]
         public void SetUp()
         {
-            rdList = Utils.GenerateRandomList(0, 20000, 10000);
+            this.rdList = Utils.GenerateRandomList(0, 20000, 10000);
 
-            //Calculate the expect distinct number of random list by build-in Distinct()
-            expectDistinct = rdList.Distinct().ToList().Count;
+            // Calculate the expect distinct number of random list by build-in Distinct().
+            this.expectDistinct = this.rdList.Distinct().ToList().Count;
         }
 
-
         /// <summary>
-        /// Test the random list size that generated from Utils class
+        /// Test the random list size that generated from Utils class.
         /// </summary>
         [Test]
         public void TestRandomListSize()
         {
-            int size = rdList.Count;
+            int size = this.rdList.Count;
             Assert.AreEqual(10000, size);
         }
-
 
         /// <summary>
         /// Test the range of random list generated from Utils class.
@@ -51,67 +50,65 @@ namespace HW2UnitTests
         public void TestRandomListRange()
         {
             bool isRange = false;
-            if (rdList.Min() >= 0 && rdList.Max() <= 20000)
+            if (this.rdList.Min() >= 0 && this.rdList.Max() <= 20000)
             {
                 isRange = true;
             }
+
             Assert.That(isRange);
         }
-
 
         /// <summary>
         /// To test some self-define list by define arry in TestCases and convert to List inside the test method.
         /// Because the TestCase only support the compile time value, where "new" a list happens in the runtime.
         /// </summary>
-        /// <param name="array"></param>
-        /// <returns></returns>
+        /// <param name="array"> pass in an array. </param>
+        /// <returns> return a number of distinct value. </returns>
         [TestCase(new[] { 1 }, ExpectedResult = 1)]
         [TestCase(new[] { 1, 2 }, ExpectedResult = 2)]
         [TestCase(new[] { 1, 2, 1, 1, 1 }, ExpectedResult = 2)]
-        [TestCase(new[] { 0,0,0,0,0,0,0,0}, ExpectedResult = 1)]
-        public int TestHashSetDistinct_SampleLists(int [] array)
+        [TestCase(new[] { 0, 0, 0, 0, 0, 0, 0, 0 }, ExpectedResult = 1)]
+        public int TestHashSetDistinct_SampleLists(int[] array)
         {
             List<int> rdList = array.ToList();
             return Utils.HashSetDistinct(rdList);
         }
 
-
         /// <summary>
         /// Test the Utils.HashSetDistinct by a random list generated from Utils.GenerateRandomList;
-        /// The expectDistinct calculated from the Distinct() build-in function.
+        /// The expect result calculated from the Distinct() build-in function.
         /// </summary>
         [Test]
         public void TestHashSetDistinct_RandomList()
         {
-            int actualDistinct = Utils.HashSetDistinct(rdList);
-            Assert.AreEqual(expectDistinct, actualDistinct);
+            int actualDistinct = Utils.HashSetDistinct(this.rdList);
+            Assert.AreEqual(this.expectDistinct, actualDistinct);
         }
 
         /// <summary>
         /// Test the correctness of Utils.ConstantSpaceDistinct by some self define list and expectedResults.
         /// </summary>
-        /// <param name="array"></param>
-        /// <returns></returns>
+        /// <param name="array"> pass in an array. </param>
+        /// <returns> return a number of distinct value. </returns>
         [TestCase(new[] { 1 }, ExpectedResult = 1)]
         [TestCase(new[] { 1, 2 }, ExpectedResult = 2)]
         [TestCase(new[] { 1, 2, 1, 1, 1 }, ExpectedResult = 2)]
         [TestCase(new[] { 0, 0, 0, 0, 0, 0, 0, 0 }, ExpectedResult = 1)]
-        public int TestConstantSpaceDistinct_SampleLists(int [] array)
+        public int TestConstantSpaceDistinct_SampleLists(int[] array)
         {
             List<int> rdList = array.ToList();
             return Utils.ConstantSpaceDistinct(rdList);
         }
 
-
+        /// <summary>
+        /// Test the Utils.ConstantSpaceDistinct by a random list generated from Utils.GenerateRandomList;
+        /// The expect result calculated from the Distinct() build-in function.
+        /// </summary>
         [Test]
         public void TestConstantSpaceDistinct_RandomList()
         {
-            int actualDistinct = Utils.ConstantSpaceDistinct(rdList);
-            Assert.AreEqual(expectDistinct, actualDistinct);
+            int actualDistinct = Utils.ConstantSpaceDistinct(this.rdList);
+            Assert.AreEqual(this.expectDistinct, actualDistinct);
         }
-
-
-
-
     }
 }
