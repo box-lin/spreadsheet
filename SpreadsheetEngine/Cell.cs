79d7de4e14ac97795ed5c638d8b6c92c41fab0b8
Boxiang Lin <boxiang.lin@wsu.edu>
2021-10-03 22:26:37 -0700

    Added Cell class in SpreadsheetEngine with the INotifyPropertyChanged configured

---
+++ SpreadsheetEngine/Cell.cs
@@ -0,0 +1,95 @@
+﻿// <copyright file="Cell.cs" company="Boxiang Lin - WSU 011601661">
+// Copyright (c) Boxiang Lin - WSU 011601661. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321
+{
+    /// <summary>
+    /// Abstract.
+    /// </summary>
+    public abstract class Cell : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Text inside a cell.
+        /// </summary>
+        protected string text;
+
+        /// <summary>
+        /// Value of a cell.
+        /// </summary>
+        protected string value;
+        private readonly int rowIndex;
+        private readonly char columnIndex;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cell"/> class.
+        /// </summary>
+        /// <param name="rowIndex"> rowIndex. </param>
+        /// <param name="columnIndex"> columnIndex. </param>
+        public Cell(int rowIndex, char columnIndex)
+        {
+            this.rowIndex = rowIndex;
+            this.columnIndex = columnIndex;
+        }
+
+        /// <inheritdoc/>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Gets the rowIndex.
+        /// </summary>
+        public int RowIndex
+        {
+            get
+            {
+                return this.rowIndex;
+            }
+        }
+
+        /// <summary>
+        /// Gets the columnIndex.
+        /// </summary>
+        public int ColumnIndex
+        {
+            get
+            {
+                return this.columnIndex;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the text.
+        /// </summary>
+        public string Text
+        {
+            get
+            {
+                return this.text;
+            }
+
+            set
+            {
+                if (this.text != value)
+                {
+                    this.text = value;
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Text"));
+                }
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                return this.value;
+            }
+        }
+    }
+}
