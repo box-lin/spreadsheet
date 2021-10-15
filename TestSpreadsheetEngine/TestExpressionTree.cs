f84cdd46404718013200b5157871756e4d0fb8ae
Boxiang Lin <boxiang.lin@wsu.edu>
2021-10-15 09:48:49 -0700

    TDD added more test cases to test all the functionalities for ExpressionTree

--- TestSpreadsheetEngine/TestExpressionTree.cs
+++ TestSpreadsheetEngine/TestExpressionTree.cs
@@ -1,11 +1,6 @@
 ﻿// <copyright file="TestExpressionTree.cs" company="Boxiang Lin - WSU 011601661">
 // Copyright (c) Boxiang Lin - WSU 011601661. All rights reserved.
 // </copyright>
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CptS321;
 using NUnit.Framework;
 
@@ -23,7 +18,8 @@ namespace TestSpreadsheetEngine
         /// <param name="expression"> expression to be evaluate. </param>
         /// <returns> result. </returns>
         [Test]
-        [TestCase("3+5",ExpectedResult = 8.0)]
+        [TestCase("3+5", ExpectedResult = 8.0)]
+        [TestCase("3+5+10", ExpectedResult = 18.0)]
         [TestCase("0+0", ExpectedResult = 0.0)]
         public double TestEvaluateAdd(string expression)
         {
@@ -38,6 +34,7 @@ namespace TestSpreadsheetEngine
         /// <returns> result. </returns>
         [Test]
         [TestCase("50-5", ExpectedResult = 45.0)]
+        [TestCase("50-5-10", ExpectedResult = 35.0)]
         [TestCase("0-0", ExpectedResult = 0.0)]
         public double TestEvaluateSub(string expression)
         {
@@ -52,6 +49,7 @@ namespace TestSpreadsheetEngine
         /// <returns> result. </returns>
         [Test]
         [TestCase("20*5", ExpectedResult = 100.0)]
+        [TestCase("20*5*2", ExpectedResult = 200.0)]
         [TestCase("0*0", ExpectedResult = 0.0)]
         public double TestEvaluateMul(string expression)
         {
@@ -66,13 +64,36 @@ namespace TestSpreadsheetEngine
         /// <returns> result. </returns>
         [Test]
         [TestCase("20/5", ExpectedResult = 4.0)]
-        [TestCase("0/0", ExpectedResult = 0.0)]
+        [TestCase("20/5/2", ExpectedResult = 2.0)]
+        [TestCase("5/0", ExpectedResult = double.PositiveInfinity)]
         public double TestEvaluateDiv(string expression)
         {
             ExpressionTree exp = new ExpressionTree(expression);
             return exp.Evaluate();
         }
 
+        /// <summary>
+        /// Test the Set Variable with a constant.
+        /// </summary>
+        [Test]
+        public void SetVariableTest()
+        {
+            string variable = "A5";
+            double constant = 99.5;
+            ExpressionTree exp = new ExpressionTree(variable);
+            exp.SetVariable(variable, constant);
+            Assert.AreEqual(exp.Evaluate(), constant);
+        }
 
+        /// <summary>
+        /// Test the Set Variable default value that assign to a variable.
+        /// </summary>
+        [Test]
+        public void SetVariableDefaultTest()
+        {
+            string variable = "B9";
+            ExpressionTree exp = new ExpressionTree(variable);
+            Assert.AreEqual(exp.Evaluate(), 0.0);
+        }
     }
 }
