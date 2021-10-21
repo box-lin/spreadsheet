27d9897608bdc04bb42d144b2bfe49aa101db922
Boxiang Lin <boxiang.lin@wsu.edu>
2021-10-20 20:26:09 -0700

    TDD added testcases for OpNodeFactory and skeleton code of OpNodeFactory.

---
+++ SpreadsheetEngine/OpNodeFactory.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="OpNodeFactory.cs" company="Boxiang Lin - WSU 011601661">
+// Copyright (c) Boxiang Lin - WSU 011601661. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CptS321
+{
+    /// <summary>
+    /// OpNodeFactory class to dynamically instantiate operation classes.
+    /// </summary>
+    public class OpNodeFactory
+    {
+        /// <summary>
+        /// Store.
+        /// </summary>
+        private Dictionary<char, OpNode> op;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpNodeFactory"/> class.
+        /// </summary>
+        public OpNodeFactory()
+        {
+            this.op = new Dictionary<char, OpNode>();
+        }
+
+        /// <summary>
+        /// Gets provide a get Op dictionary for testing purpose.
+        /// </summary>
+        public Dictionary<char, OpNode> Op
+        {
+            get
+            {
+                return this.op;
+            }
+        }
+
+        /// <summary>
+        /// Create the operator object.
+        /// </summary>
+        /// <param name="symbol"> char symbol of the operator. </param>
+        /// <returns> return a operator object or exception. </returns>
+        public OpNode CreateOperatorNode(char symbol)
+        {
+            if (this.op.ContainsKey(symbol))
+            {
+                return this.op[symbol];
+            }
+            else
+            {
+                throw new NotSupportedException("The operation" + symbol.ToString() + "is not supoorted");
+            }
+        }
+
+        /// <summary>
+        /// Initil the dictionary.
+        /// </summary>
+        private void InitOp()
+        {
+            //this.op['+'] = new PlusOp();
+        }
+
+    }
+}
---
+++ TestSpreadsheetEngine/TestOpNodeFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CptS321;
+using NUnit.Framework;
+
+namespace TestSpreadsheetEngine
+{
+
+    /// <summary>
+    /// Class to test the Expression Tree.
+    /// </summary>
+    [TestFixture]
+    public class TestOpNodeFactory
+    {
+        private OpNodeFactory factory = new OpNodeFactory();
+
+        /// <summary>
+        /// Test InitOp if all current exist operator objects have been filled into the dictionary.
+        /// </summary>
+        [Test]
+        public void TestInitOp()
+        {
+            HashSet<char> opSet = new HashSet<char> { '+', '-', '/', '*' };
+            int expectSum = opSet.Count;
+            int actualSum = 0;
+            foreach (char key in this.factory.Op.Keys)
+            {
+                if (opSet.Contains(key))
+                {
+                    opSet.Remove(key);
+                    actualSum++;
+                }
+            }
+
+            Assert.That(actualSum, Is.EqualTo(expectSum), "The InitOp doesn't cover all the op classes that currently provided in the namespace.");
+        }
+
+        /// <summary>
+        /// Test correctness operator object that CreateOperatorNode method returns.
+        /// </summary>
+        [Test]
+        public void TestCreateOperatorNode()
+        {
+            Assert.That(this.factory.CreateOperatorNode('+'), Is.InstanceOf<PlusOp>());
+            Assert.That(this.factory.CreateOperatorNode('-'), Is.InstanceOf<SubOp>());
+            Assert.That(this.factory.CreateOperatorNode('*'), Is.InstanceOf<MulOp>());
+            Assert.That(this.factory.CreateOperatorNode('/'), Is.InstanceOf<DivideOp>());
+        }
+
+        /// <summary>
+        /// Test the NotSupportedException that CreateIoeratorNode returns.
+        /// </summary>
+        [Test]
+        public void TestCreateOPeratorNodeException()
+        {
+            Assert.Throws<NotSupportedException>(() => this.factory.CreateOperatorNode('%'));
+            Assert.Throws<NotSupportedException>(() => this.factory.CreateOperatorNode('$'));
+        }
+    }
+}
